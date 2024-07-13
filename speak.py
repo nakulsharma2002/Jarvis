0e2eaf88c53b4983e7624a15e48ac2fe95dad92b
nakulsharma2002 <snakul437@gmail.com>
2024-07-13 16:43:06 +0530

    Initial commit

--- speak.py
+++ speak.py
@@ -2,10 +2,14 @@ import pyttsx3
 import datetime
 import speech_recognition as sr
 import time
+from mtranslate import translate
+from colorama import Fore,Style,init
+init(autoreset=True)
+
 
 # Initialize the text-to-speech engine
 engine = pyttsx3.init('sapi5')
-engine.setProperty('rate', 130)  # Set speaking rate
+engine.setProperty('rate', 240)  # Set speaking rate
 engine.setProperty('volume', 1.0)  # Set volume level (1.0 is the maximum)
 
 def speak(audio):
@@ -25,30 +29,50 @@ def wishme():
     else:
         speak('Good night, sir')
 
+
+def Trans_hindi_to_english(txt):
+    english_Txt = translate(txt,to_language="en-in")
+    return english_Txt
+
+
 def takecommand():
     """Listen to the user's command and convert it to text."""
     r = sr.Recognizer()
+    # Adjust for ambient noise with dynamic energy threshold
+    r.dynamic_energy_threshold = False
+    r.energy_threshold =35000
+    r.dynamic_energy_adjustment_damping = 0.03
+    r.dynamic_energy_ratio = 1.9
+    r.operation_timeout = None
+    r.non_speaking_duration = 0.3
+    r.pause_threshold = 0.5  # Set pause threshold
     with sr.Microphone() as source:
-        print("Listening...")
-        # Adjust for ambient noise with dynamic energy threshold
-        r.dynamic_energy_threshold = True
-        r.adjust_for_ambient_noise(source, duration=1)
-        r.pause_threshold = 1  # Set pause threshold
-        audio = r.listen(source)
-    try:
-        print("Recognizing...")
-        query = r.recognize_google(audio, language='en-in')
-        print(f"User said: {query}\n")
-    except sr.UnknownValueError:
-        print("I didn't catch that. Please say it again.")
-        return "None"
-    except sr.RequestError:
-        print("Couldn't request results; check your network connection.")
-        return "None"
-    return query
+        r.adjust_for_ambient_noise(source)
+        while True:
+            print(Fore.LIGHTGREEN_EX + "I am Listening...")
+            try:
+                audio = r.listen(source,timeout=None)
+                # print(Fore.LIGHTYELLOW_EX + "Got it Now Recognizing",end=" ",flush=True)
+                query = r.recognize_google(audio, language='en-in')
+                # print(Fore.LIGHTGREEN_EX,f"User said: {audio}\n",flush=True)
+                if query:
+                    translated_txt = Trans_hindi_to_english(query)
+                    print("\r"+Fore.BLUE + "Mr Nakul : ", translated_txt)
+                    
+                else:
+                    return ""
+            
+            except sr.UnknownValueError:
+                print(Fore.LIGHTYELLOW_EX + "I didn't catch that. Please say it again.")
+                return "None"
+            except sr.RequestError:
+                print(Fore.LIGHTYELLOW_EX + "Couldn't request results; check your network connection.")
+                return "None"
+            
+            return translated_txt
 
 if __name__ == '__main__':
-    wishme()
+    
     while True:
         command = takecommand()
         if command.lower() in ["exit", "quit", "stop"]:
